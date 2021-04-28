e39e35e78b97fec903dc45dd50aee00b1cb5f676
zuoguipeng <zuoguipeng@p1.com>
2021-04-28 18:33:45 +0800

    fix and add func

--- bitmap.go
+++ bitmap.go
@@ -5,42 +5,115 @@ import (
 )
 
 type BitMap struct {
-	data      []byte
-	bitSize   uint32
-	maxOffset uint32
+	data     []byte
+	bitSize  uint32
+	byteSize int
 }
 
 func NewBitMap(size uint32) *BitMap {
 	if size%8 > 0 {
 		size += 8 - size%8
 	}
+	byteSize := size >> 3
 	return &BitMap{
-		bitSize: size,
-		data:    make([]byte, size>>3),
+		data:     make([]byte, byteSize),
+		bitSize:  size,
+		byteSize: int(byteSize),
 	}
 }
 
 func (bm *BitMap) SetBit(offset uint32) {
+	if offset > bm.bitSize {
+		return
+	}
+	offset--
 	index, pos := offset>>3, offset%8
 	bm.data[index] |= 0x01 << pos
-	if bm.maxOffset < offset {
-		bm.maxOffset = offset
-	}
 }
 
 func (bm *BitMap) GetBit(offset uint32) uint8 {
+	if offset > bm.bitSize {
+		return 0
+	}
+	offset--
 	index, pos := offset>>3, offset%8
 	return bm.data[index] >> pos & 0x01
 }
 
-func (bm *BitMap) GetData() string {
+func (bm *BitMap) ClearBit(offset uint32) {
+	if offset > bm.bitSize {
+		return
+	}
+	offset--
+	index, pos := offset>>3, offset%8
+	bm.data[index] &^= 0x01 << pos
+}
+
+func (bm *BitMap) Clone() *BitMap {
+	bmCopy := NewBitMap(bm.bitSize)
+	copy(bmCopy.data, bm.data)
+	return bmCopy
+}
+
+func (bm *BitMap) And(other *BitMap) *BitMap {
+	size := min(bm.bitSize, other.bitSize)
+	res := NewBitMap(size)
+	for i := 0; i < res.byteSize; i++ {
+		res.data[i] = bm.data[i] & other.data[i]
+	}
+
+	return res
+}
+
+func (bm *BitMap) Or(other *BitMap) *BitMap {
+	var res, s *BitMap
+	if bm.byteSize > other.byteSize {
+		res = bm.Clone()
+		s = other
+	} else {
+		res = other.Clone()
+		s = bm
+	}
+	for i := 0; i < res.byteSize; i++ {
+		res.data[i] |= s.data[i]
+	}
+
+	return res
+}
+
+func (bm *BitMap) AndNot(other *BitMap) *BitMap {
+	bmCopy := bm.Clone()
+	for i := 0; i < bmCopy.byteSize; i++ {
+		bmCopy.data[i] &^= other.data[i]
+	}
+
+	return bmCopy
+}
+
+func (bm *BitMap) ToString() string {
 	res := ""
-	for i := bm.maxOffset; i > 0; i-- {
+	for i := bm.bitSize; i > 0; i-- {
 		res += strconv.Itoa(int(bm.GetBit(i)))
 	}
 	return res
 }
 
-func (bm *BitMap) GetSize() uint32 {
+func (bm *BitMap) GetBitSize() uint32 {
 	return bm.bitSize
 }
+
+func (bm *BitMap) GetByteSize() int {
+	return bm.byteSize
+}
+
+func (bm *BitMap) GetData() []byte {
+	return bm.data
+}
+
+func min(a, b uint32) uint32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
--- bitmap_test.go
+++ bitmap_test.go
@@ -8,10 +8,38 @@ import (
 
 func TestBitMap(t *testing.T) {
 	bm := bitmap.NewBitMap(35)
-	fmt.Println(bm.GetSize())
 	bm.SetBit(5)
 	bm.SetBit(2)
 	fmt.Println(bm.GetBit(3))
 	fmt.Println(bm.GetBit(5))
-	fmt.Println(bm.GetData())
+	bm.ClearBit(5)
+	fmt.Println(bm.GetBit(5))
+
+	bm1 := bitmap.NewBitMap(10)
+	bits := []int{1, 3, 4, 5, 8, 9, 10}
+	for _, v := range bits {
+		bm1.SetBit(uint32(v))
+	}
+	fmt.Printf("bm1: %s\n", bm1.ToString())
+	fmt.Printf("bm1: data:%v\n", bm1.GetData())
+
+	bm2 := bitmap.NewBitMap(10)
+	bits = []int{1, 3, 4, 6, 8, 9, 10}
+	for _, v := range bits {
+		bm2.SetBit(uint32(v))
+	}
+	fmt.Printf("bm2: %s\n", bm2.ToString())
+	fmt.Printf("bm2: data:%v\n", bm2.GetData())
+
+	bm3 := bm1.And(bm2)
+	fmt.Printf("b1 b2 交集: %s\n", bm3.ToString())
+	fmt.Printf("bm3: data:%v\n", bm3.GetData())
+
+	bm3 = bm1.Or(bm2)
+	fmt.Printf("b1 b2 并集: %s\n", bm3.ToString())
+	fmt.Printf("bm3: data:%v\n", bm3.GetData())
+
+	bm3 = bm1.AndNot(bm2)
+	fmt.Printf("b1 b2 差集: %s\n", bm3.ToString())
+	fmt.Printf("bm3: data:%v\n", bm3.GetData())
 }
